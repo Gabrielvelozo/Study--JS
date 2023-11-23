b5f1249d3b80cb20289b382e4d9037042726182a
GabrielVelozo <gabittovelozo@gmail.com>
2023-11-23 05:21:37 -0300

    [Javascript DM] string

--- Javascript/string.js
+++ Javascript/string.js
@@ -1,20 +1,9 @@
-/* 
-*  Confeccionar una función que retorne si un string es capicua, es decir si se lee igual de izquierda a derecha o derecha a izquierda
-*/
-
-function esCapicua(parametro){
-    
-}
-
-
-
-
 
 /* 
 * Confeccionar una función que retorne la cantidad de letras 'a' o 'A' tiene una palabra.
 */
 
-let palabra=prompt('Ingresar una palabra:');
+/* let palabra=prompt('Ingresar una palabra:');
 
 function letrasA (parametro){
     let contadorA=0;
@@ -25,4 +14,46 @@ function letrasA (parametro){
     }
    document.write(`Contador= ${contadorA}`);
 }
-letrasA(palabra);
+letrasA(palabra); */
+
+/* 
+* Se tiene un array con un conjunto de objetos que representan personas.
+* Confeccionar una función qeu reciba el array 'personas' y parte del inicio del nombre a buscar.
+* Buscar e imprimir todas las personas que comienzan con dicha cadena (mostrar indistintamente si es mayúscula o minúscula), resolver el problema de tres formas:
+* - Accediendo al array por medio de subindices.
+* - Recorriendo el array mediante el for of.
+* - Filtrando el array mediante el método filter().
+*/
+const personas =[
+    {
+        nombre: 'Juan Perez',
+        edad: 30,
+    },
+    {
+        nombre: 'Maria Lopez',
+        edad: 25,
+    },
+    {
+        nombre: 'Pedro Martinez',
+        edad: 35,
+    },
+    {
+        nombre: 'Peña Lara',
+        edad: 28,
+    }
+]
+function persona (text){
+    // filter()
+    //personas.filter(argumento => argumento.nombre == parámetro);
+
+    // for of
+    for( let parte of personas){
+        if(parte.nombre == text){
+            console.log(`${parte.nombre}`);
+        }
+    }
+}
+
+console.log(persona('pe'));
+
+//212
