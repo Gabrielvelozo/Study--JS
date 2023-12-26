40b9d6211d73ee3901e706373441bf1d62964ace
GabrielVelozo <gabittovelozo@gmail.com>
2023-12-26 07:17:44 -0300

    +

--- Javascript/Dom.js
+++ Javascript/Dom.js
@@ -602,7 +602,7 @@ color.forEach(recuadro =>{
  * Si ingresa en forma correcta ahora mostrar otro numero con un dígito mas.
  * Finalizar la aplicación cuando el usuario se equivoca.
  */
-const mostrar= document.querySelector('#mostrar');
+/* const mostrar= document.querySelector('#mostrar');
 const numero= document.querySelector('#numero');
 let nRandom= Math.floor(Math.random()*9)+1;
 mostrar.textContent = `${nRandom}`
@@ -614,4 +614,33 @@ document.querySelector('#confirmar').addEventListener('click',()=>{
     if(numero.value== nRandom){
         mostrar.style.visibility= `visible`;
     }
-})
+}) */
+
+/* 
+* Mostrar un mensaje publicitario donde aparezcan las letras en forma lenta en pantalla. 
+* Cuando ya se haya mostrado el mensaje en forma completa, esperar 3 segundos, borrar la pantalla
+* y volver a mostrar el mensaje.
+*/
+/* const anuncio = 'Espacio publicitario';
+const mensaje= document.querySelector('#mensaje');
+let cantidad= 1;
+let tiempo= setInterval(() => {
+    mostrarAnuncio();
+}, 100);
+
+function mostrarAnuncio(){
+    const texto= anuncio.slice(0,cantidad);
+    mensaje.textContent = texto;
+    if(cantidad > anuncio.length){
+        clearInterval(tiempo);
+        setTimeout(() => {
+            mensaje.textContent= '';
+            cantidad= 1;
+            tiempo= setInterval(()=>{
+                mostrarAnuncio();
+            },100)
+        }, 3000);
+    }
+    cantidad++;
+}
+ */
