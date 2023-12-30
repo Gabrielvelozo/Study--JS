824b91679c8867616f26560be1d24f44fdfce8d8
GabrielVelozo <gabittovelozo@gmail.com>
2023-12-30 06:49:08 -0300

    [Javascript] APi

--- Javascript/ApiFetch.js
+++ Javascript/ApiFetch.js
@@ -12,7 +12,7 @@ setInterval(() => chistes(), 5000); */
 * a todas las provincias y al pasar el mouse sobre el enlace proceder a mostrar su temperatura en un párrafo.
 */
 
-const provincias = [
+/* const provincias = [
     "Buenos Aires",
     "Catamarca",
     "Chaco",
@@ -52,4 +52,27 @@ function recuperarTemperatura(prov){
     fetch(prov)
     .then(respuesta=> respuesta.json())
     .then(provincia => document.querySelector('#resultado').textContent= `Provincia: ${provincia.provincia}, Temperatura: ${provincia.temperatura}`)
-}
+} */
+
+/* 
+* Crear un formulario que permita consultar todos los artículos cuyo precio se encuentra comprendido entre un monto mínimo y el monto máximo
+* Luego al presionar un boton de tipo submit proceda a enviar el formulario y retorne un arreglo JSON con todos los productos que tengan un precio
+* comprendido entre los dos montos.
+*/
+
+const formulario= document.querySelector('#formulario');
+const resultado= document.querySelector('#resultado');
+
+formulario.addEventListener('submit', evento => {
+evento.preventDefault();
+const datosFormulario= new FormData(formulario);
+fetch('https://www.scratchya.com.ar/cursojs/consultarpormonto.php',{
+    method:'POST',
+    body: datosFormulario
+})
+.then(respuesta => respuesta.json())
+.then(articulos => {
+const lista= articulos.map(articulo =>`<li>${articulo.nombre} = ${articulo.precio}`);
+resultado.innerHTML= lista;
+})
+})
